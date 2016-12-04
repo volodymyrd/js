bc34f35c4caf273176d3d1f94f6034f41582c146
volodymyrd <volodymyrdotsenko@gmail.com>
2016-12-04 21:32:40 +0100

    form

--- angular2/pony/src/app/app.module.ts
+++ angular2/pony/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import {RegisterFormComponent} from "./components/user/register-form.component";
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
--- angular2/pony/src/app/components/user/register-form.component.ts
+++ angular2/pony/src/app/components/user/register-form.component.ts
@@ -1,9 +1,47 @@
 import {Component} from "@angular/core";
+import {FormGroup, FormControl, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
-    selector: 'ns-register',
-    templateUrl: 'register-form.component.html',
+  selector: 'ns-register',
+  templateUrl: 'register-form.component.html',
 })
 export class RegisterFormComponent {
+  userFormGroup: FormGroup;
+  passwordFormGroup: FormGroup;
+  username: FormControl;
+  password: FormControl;
+  passwordConfirm: FormControl;
+  //email: FormControl;
+  passwordStrength: number = 0;
 
+
+  constructor(fb: FormBuilder) {
+    this.username = fb.control('', Validators.required);
+    this.password = fb.control('', Validators.required);
+    this.passwordConfirm = fb.control('', Validators.required);
+
+    this.passwordFormGroup = fb.group({
+        password: this.password,
+        passwordConfirm: this.passwordConfirm
+      },
+      {validator: RegisterFormComponent.passwordMatch}
+    );
+    this.userFormGroup = fb.group({
+        username: this.username,
+        passwordFormGroup: this.passwordFormGroup
+      }
+    );
+
+  }
+
+  static passwordMatch(control: FormGroup) {
+    let password = control.controls['password'].value;
+    let confirm = control.controls['passwordConfirm'].value;
+
+    return password === confirm ? null : {matchingError: true};
+  }
+
+  register() {
+    console.log(this.userFormGroup.value);
+  }
 }
