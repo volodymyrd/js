c1bca2ea6a792ae7be236518518633dd97fcb147
volodymyrd <volodymyrdotsenko@gmail.com>
2016-12-07 20:21:42 +0100

    master/details

--- angular2/heroes/src/app/app.component.ts
+++ angular2/heroes/src/app/app.component.ts
@@ -1,6 +1,19 @@
 import { Component } from '@angular/core';
 import {Hero} from "./model/hero";
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +21,11 @@ import {Hero} from "./model/hero";
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero){
+    this.selectedHero = hero;
+    console.log(hero);
+  }
 }
